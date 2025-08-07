ab89122c2e888ffd78c0b2d2dd7a02b1b841476d
bhanu <bhanutejamakkineni@gmail.com>
2025-08-07 14:42:21 +0530

    used gemini api for chatbot

--- API/app.py
+++ API/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from langchain.prompts import ChatPromptTemplate
 from langchain_ollama import ChatOllama
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.llms import Ollama
 from langserve import add_routes
 import uvicorn
@@ -11,29 +12,44 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = FastAPI(
-    title="TinyLlama Chatbot API",
+    title="Simple Chatbot API",
     description="A simple API for interacting with the TinyLlama chatbot using LangChain and Ollama.",
     version="1.0.0"
 )
 
+
+
+
 llm1 = ChatOllama(model="tinyllama")
-
-add_routes(
-    app,
-    llm1,
-    path="/tinyllama"
+gemini = ChatGoogleGenerativeAI(
+    model="models/gemini-1.5-flash",
+    google_api_key=os.getenv("GEMINI_API_KEY")
 )
 
-llm = Ollama(model = "tinyllama")
+
+
 prompt1 = ChatPromptTemplate.from_template(
-    "Write a essay about {topic} with a 100 words"
+    "You are a helpful assitant prompt : {topic}."
+)
+prompt2 = ChatPromptTemplate.from_template(
+    "You are a helpful assistant prompt : {topic}."
+)
+
+
+
+add_routes(
+    app,
+    prompt1 | llm1,
+    path="/ollama"
 )
 
 add_routes(
     app,
-    prompt1 | llm,
-    path="/essay"
+    prompt2 | gemini,
+    path="/gemini"
 )
+
+
 
 if __name__ == "__main__":
     uvicorn.run(app,host="localhost", port=8000, log_level="info")
--- API/client.py
+++ API/client.py
@@ -3,13 +3,26 @@ import streamlit as st
 
 def get_tinyllama_response(prompt):
     response = requests.post(
-        "http://localhost:8000/essay/invoke",
+        "http://localhost:8000/ollama/invoke",
         json={"input": {'topic':prompt}}
     )
     return response.json()['output']
 
+def get_gemini_response(prompt):
+    response = requests.post(
+        "http://localhost:8000/gemini/invoke",
+        json={"input": {'topic':prompt}}
+    )
+    return response.json()['output']['content']
+
 st.title("TinyLlama Chatbot")
-input_text = st.text_input("Enter a topic for the essay:")
+input_text = st.text_input("Enter a prompt for TinyLlama:")
+
+st.title("Gemini Chatbot")
+input_text1 = st.text_input("Enter a prompt for Gemini:")
 
 if input_text:
     st.write(get_tinyllama_response(input_text))
+
+if input_text1:
+    st.write(get_gemini_response(input_text1))
